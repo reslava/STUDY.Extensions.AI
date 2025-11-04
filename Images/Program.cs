80a8063e64249d920d63bc17baf66c8fe2c4942a
reslava <rafeslava@hotmail.com>
2025-11-04 17:12:11 +0100

    Add project files.

---
+++ Images/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using OllamaSharp;
+
+var builder = Host.CreateApplicationBuilder ();
+
+var client = builder.Services.AddChatClient (new OllamaApiClient (new Uri ("http://localhost:11434"), "gemma3:4b")); 
+client.UseDistributedCache
+    (new MemoryDistributedCache (Options.Create (new MemoryDistributedCacheOptions ())));
+
+var host = builder.Build ();
+
+var chatClient = host.Services.GetRequiredService<IChatClient> ();
+
+foreach (var filePath in Directory.GetFiles ("images", "*.png"))
+{
+    var name = Path.GetFileNameWithoutExtension (filePath);
+    var message = new ChatMessage (ChatRole.User,
+        $"""
+        Analyze the image and identify all objects present in it.
+        Provide the status of traffic, number of cars, trucks, motorbikes, Cyclists and pedestrians.        
+        """);
+    message.Contents.Add (new DataContent (File.ReadAllBytes (filePath), "image/png"));
+    var imagesResponse = await chatClient.GetResponseAsync<ImagesResult> (message);
+    Console.WriteLine (imagesResponse.Result with { CameraName = name });
+}
+
+
+public record ImagesResult
+{
+    public string CameraName { get; init; }
+    public string Objects { get; init; }
+    public TrafficStatus Status { get; init; }
+    public int Cars { get; init; }
+    public int Trucks { get; init; }
+    public int Motorbikes { get; init; }
+    public int Cyclists { get; init; }
+    public int Pedrestians { get; init; }
+
+    public enum TrafficStatus
+    {
+        Empty,
+        Low,
+        Medium,
+        High
+    }
+}
---
+++ LLM/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using OllamaSharp;
+
+var builder = Host.CreateApplicationBuilder ();
+
+var client = builder.Services.AddChatClient (new OllamaApiClient (new Uri ("http://localhost:11434"), "llama3.1")); //llama3.1   phi3:mini
+client.UseDistributedCache 
+    (new MemoryDistributedCache(Options.Create (new MemoryDistributedCacheOptions ())));
+
+var host = builder.Build ();
+
+var chatClient = host.Services.GetRequiredService<IChatClient> ();
+
+ChatOptions statefulOptions = new () { ConversationId = "my-conversation-id" };
+while (true)
+{
+    Console.Write ("Q: ");
+    ChatMessage message = new (ChatRole.User, Console.ReadLine ());
+    Console.WriteLine (statefulOptions.ConversationId);
+    Console.WriteLine (await chatClient.GetResponseAsync (message, statefulOptions));
+}
+
+//Console.WriteLine (await client.GetResponseAsync (
+//[
+//    new(ChatRole.System, "You are a helpful AI assistant"),
+//    new(ChatRole.User, "What is AI?"),
+//]));
+
+//List<ChatMessage> history = [];
+//while (true)
+//{
+//    Console.Write ("Q: ");
+//    history.Add (new (ChatRole.User, Console.ReadLine ()));
+
+//    ChatResponse response = await client.GetResponseAsync (history);
+//    Console.WriteLine (response);
+
+//    history.AddMessages (response);
+//}
